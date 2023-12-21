8ac4c36957954c34d4f66127fa620e031bf6265c
khanhbq_he2002 <khanhbui1560@gmail.com>
2023-12-21 12:08:22 +0700

    Add game state & player state machine

---
+++ Assets/_Game/Scripts/Enums/PlayerTaskType.cs
@@ -0,0 +1,7 @@
+public enum PlayerTaskType : byte
+{
+    None = 0,
+    RollDice = 1,
+    BuyProperty = 2,
+    ChooseProperty = 3
+}
--- Assets/_Game/Scripts/Player/PlayerInput.cs
+++ Assets/_Game/Scripts/Player/PlayerInput.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private IPlayerInputState _currentState;
+    private Player _player;
+
+    private void Start()
     {
-        
+        _player = GetComponent<Player>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_currentState != null)
+        {
+            _currentState.OnExecute(_player);
+        }
+    }
+
+    public void SetState(IPlayerInputState state)
+    {
+        if (_currentState != null)
+        {
+            _currentState.OnExit(_player);
+        }
+        _currentState = state;
+        _currentState.OnInitialize(_player);
     }
 }
